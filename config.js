f1edc2bb5356f31c9df7a32f278a3485c09162b5
minecraft8472025 <minecraft8472025@outlook.com>
2025-03-20 14:52:30 +0000

    Changed Gal's password

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
     // You can add multiple users by doing username: 'password'.
     PikachuFan847: "PIKACHU!*)(&!",
     MoltenVc3: "Turkey12!",
-    dogishknight06: "Gal",
+    dog_16: "Gal",
   },
 };
 
