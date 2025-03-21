c750115dd136f86d319ae54c5662f7e3df138125
minecraft8472025 <minecraft8472025@outlook.com>
2025-03-21 06:58:37 -0400

    Update config.js; Removed password

--- config.js
+++ config.js
@@ -1,13 +1,8 @@
 const config = {
-  challenge: true, // Set to true if you want to enable password protection.
+  challenge: false, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    PikachuFan847: "PIKACHU!*)(&!",
-    MoltenVc3: "Turkey12!",
-    dog_16: "Gal",
-    Hayden: "Mewisthebest1264",
-    Gwen: "Gwen",
-    Ava: "cuteKitty",
+    
   },
 };
 
